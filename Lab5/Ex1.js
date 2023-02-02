0da2dab6fba8b6945b3c5ec1d29fae581d1e6121
calebjchang <caleb33@hawaii.edu>
2023-02-02 13:17:35 -1000

    Lab4&5 progress

---
+++ Lab5/Ex1.js
@@ -0,0 +1,9 @@
+age =  19;
+fav_num = 11;
+day_of_birth = 13;
+month_of_birth = 9;
+
+console.log(
+    age + fav_num / day_of_birth * month_of_birth
+);
+console.log((age + fav_num) / day_of_birth * month_of_birth);
