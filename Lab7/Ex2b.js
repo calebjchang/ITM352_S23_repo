c173f022070ae7bdf46fd16f3c9d28180366d49b
calebjchang <caleb33@hawaii.edu>
2023-02-20 12:19:20 -1000

    LAB7
    WIP

---
+++ Lab7/Ex2b.js
@@ -0,0 +1,22 @@
+require("./products_data.js");
+
+var num_products = 5;
+var prod_number = 0;
+
+while(prod_number < num_products){
+    prod_number++;
+    if(prod_number > 0.25*num_products && prod_number < 0.75*num_products){
+        console.log(`${eval("name" + prod_number)} is sold out`)
+        continue;
+    }
+    console.log(`${prod_number} . ${eval('name' + prod_number)}`);
+    
+/*
+if(prod_number > num_products/2) {
+    console.log("That's enough!");
+    break;
+    }
+    */
+}
+
+console.log("That's all we have.")
